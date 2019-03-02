bc44578490c9c5b0f7b84fea296c529be0d8245a
Julia Denysenko <julia@mac.TRIOLAN>
2019-03-02 22:16:12 +0200

    changes

--- testing_atm.py
+++ testing_atm.py
@@ -2,33 +2,48 @@ import unittest
 import base_money
 
 class TestRise(unittest.TestCase):
-    atm_balance = base_money.Atm.atm_balance
+    Atm = base_money.Atm()
+
+    def SetUp(self):
+      self.Atm = base_money.Atm()
 
     def test_rise_1(self):
-        self.assertEqual(base_money.Atm.atm_balance, 10000)
+        self.assertEqual(self.Atm.atm_balance, 10000)
 
     def test_rise_2(self):
-        self.assertEqual(base_money.Atm.rise_money(self, 100), 10100)
+        self.assertEqual(self.Atm.rise_money(100), 10100)
 
     def test_rise_3(self):
-        self.assertEqual(base_money.Atm.rise_money(self, -100), 9900) #это имеет смысл?нас интересует же пополнение...
+        self.assertEqual(self.Atm.rise_money(-100), 9900)
 
     def test_rise_4(self):
-        self.assertEqual(base_money.Atm.rise_money(self, 0), 10000)
+        self.assertEqual(self.Atm.rise_money(0), 10000)
 
     def test_rise_5(self):
-        self.assertEqual(base_money.Atm.atm_balance, 100001)
+        self.assertEqual(self.Atm.atm_balance, 100001)
 
     def test_rise_6(self):
-        self.assertEqual(base_money.Atm.atm_balance, 9999)
+        self.assertEqual(self.Atm.atm_balance, 9999)
 
     def test_rise_7(self):
-        self.assertEqual(base_money.Atm.atm_balance, -10000)
+        self.assertEqual(self.Atm.atm_balance, -10000)
 
     def test_rise_8(self):
-        self.assertEqual(base_money.Atm.atm_balance, 0)
+        self.assertEqual(self.Atm.atm_balance, 0)
 
-  
+class TestPIN(unittest.TestCase):
+    Atm = base_money.Atm()
+
+    def SetUp(self):
+        self.Atm = base_money.Atm()
+
+    def test_pin_1(self):
+        self.assertTrue(self.Atm.enter_pin(777))
+
+    def test_pin_2(self):
+        self.assertRaises(base_money.IncorrectPin, self.Atm.enter_pin, 111)
+
+
 
 
 unittest.main()
